278282bc930cb0b1945f5330cd3714724d5c0697
Simon Lutz <lutz.simon1900@gmail.com>
2024-05-30 12:12:55 +0200

    Initial pus - First draw of the script

---
+++ header_template.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# SYNOPSIS
+#    
+# DESCRIPTION
+#    This is a script template
+#    to start any good shell script.
+#
+# OPTIONS
+#
+# EXAMPLES
+#    ${SCRIPT_NAME} -o DEFAULT arg1 arg2
+#
+#===========================================================
+# IMPLEMENTATION
+#    version         ${SCRIPT_NAME} 0.0.1
+#    author          Simon
+#    license         GNU GENERAL PUBLIC LICENSE Version 3
+############################################################
+# Help                                                     #
+############################################################
+Help()
+{
+   # Display Help
+   echo "Add description of the script functions here."
+   echo
+   echo "Syntax: scriptTemplate [-g|h|v|V]"
+   echo "options:"
+   echo "g     Print the GPL license notification."
+   echo "h     Print this Help."
+   echo "v     Verbose mode."
+   echo "V     Print software version and exit."
+   echo
+}
+
+
+############################################################
+# Main program                                             #
+############################################################
+
+
+
+############################################################
+# Process the input options. Add options as needed.        #
+############################################################
+# Get the options
+while getopts ":h" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+   esac
+done
+
+echo "Hello world!"
